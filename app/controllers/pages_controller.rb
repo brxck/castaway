e180930dfe3218f46673a42ed0ebfc3a0cc0b135
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-26 12:40:39 -0700

    Create discover page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,10 @@ class PagesController < ApplicationController
   end
 
   def discover
-    @toplist = Gpod.toplist(5)
+    rss = "https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json?at=10l9W2"
+    results = Connect.get(rss).body["feed"]["results"]
+
+    @toplist = results.map { |podcast| OpenStruct.new(podcast) }
   end
 
   def search
