80e452f974b3526d02479ab1384dfd1f62590c2b
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-27 07:26:15 -0700

    Add pagination to episodes

--- app/controllers/podcast_controller.rb
+++ app/controllers/podcast_controller.rb
@@ -1,12 +1,14 @@
 class PodcastController < ApplicationController
   include ItunesHelper
   include FeedHelper
+  include Pagy::Backend
 
   def show
     @podcast = Itunes.lookup(params[:id])
 
     xml = Connect.get(@podcast.feedUrl).body
     @feed = Feed.parse(xml)
+    @pagy, @episodes = pagy_array(@feed.rss.channel.items)
   end
 
   def listen
