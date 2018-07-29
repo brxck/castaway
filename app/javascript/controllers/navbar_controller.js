f443fd70d8c52ceae839a317927723df8edd4b45
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-29 16:34:13 -0700

    Refactor

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["button", "menu"]
 
-  toggle_menu() {
+  toggleMenu() {
     this.buttonTarget.classList.toggle("is-active")
     this.menuTarget.classList.toggle("is-active")
   }
