5c853475b8f8783837f5951c4c3fd4b30dc2b455
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-29 17:29:56 -0700

    Fix play pause button

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -112,7 +112,7 @@ export default class extends Controller {
   }
 
   playing() {
-    !this.audioTarget.paused
+    return !this.audioTarget.paused
   }
 
   // Scrub value is the fraction of played audio
