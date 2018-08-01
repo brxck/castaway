d141a3c80c52fb3b01beb3f797bddb90edc84a25
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-07-31 20:26:06 -0700

    Refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   include ItunesHelper
+  include PodcastHelper
   include Pagy::Backend
   
   def home
@@ -9,10 +10,19 @@ class PagesController < ApplicationController
     rss = "https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json?at=10l9W2"
     results = Connect.get(rss).body["feed"]["results"]
 
-    @toplist = results.map { |podcast| OpenStruct.new(podcast) }
+    @toplist = results.map do |podcast|
+      OpenStruct.new(
+        itunes_id: podcast["id"],
+        name: podcast["name"],
+        art100: podcast["artworkUrl100"],
+        art600: podcast["artworkUrl600"]
+      )
+    end
   end
 
   def search
-    @pagy, @results = pagy_array(Itunes.search(params[:q]))
+    results = Itunes.search(params[:q])
+    @results = results.map { |podcast| process_podcast(podcast) }
+    @pagy, @results = pagy_array(@results)
   end
 end
