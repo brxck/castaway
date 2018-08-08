4919cfe4f65adb7bfb681bb6b1a7b54c2c7f3107
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-08 16:28:36 -0700

    Fix last played bug for real

--- app/helpers/last_played_helper.rb
+++ app/helpers/last_played_helper.rb
@@ -10,11 +10,16 @@ module LastPlayedHelper
     last_podcast_path + "?episode_id=#{@last_played['episode_id']}"    
   end
 
+  def try_last(key)
+    return unless @last_played
+    @last_played[key]
+  end
+
   def last_art_url
-    @last_played["art_url"] || ""
+    @last_played.try(:[], "art_url")
   end
 
   def last_audio_url
-    @last_played["episode_url"] || ""
+    @last_played.try(:[], "episode_url")
   end
 end
