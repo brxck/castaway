22cdf4abbc8e12266a921c63c390481b831a573e
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-08 11:55:29 -0700

    Create narrow layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :set_last_played
+  layout :devise_layout
 
   private
 
   def set_last_played
     return unless cookies[:last_played]
     @last_played = JSON.parse(cookies[:last_played])
+  end
+
+  def devise_layout
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
