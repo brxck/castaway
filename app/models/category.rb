7e6415451f1dd1a2e58532029836650331cc6b1c
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-11 15:50:32 -0700

    Implement category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :category, foreign_key: "parent_id", optional: true
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id"
+
+  def podcasts
+    Itunes.genre(id)
+  end
 end
