ec61ab11e46928254302052d3620f344a8687d75
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-11 12:03:40 -0700

    Load categories from itunes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
 # User for development
 if Rails.env.development?
-  user = User.create(email: "test@example.com", password: "hunter2")
-  user.subscriptions.create(itunes_id: 947899573, # rubocop:disable Style/NumericLiterals
+  user = User.create!(email: "test@example.com", password: "hunter2")
+  user.subscriptions.create!(itunes_id: 947899573, # rubocop:disable Style/NumericLiterals
                             name: "The Adventure Zone",
                             feed: "http://adventurezone.libsyn.com/rss")
 end
+
+# Load iTunes genres into database
+genres_url = "https://itunes.apple.com/WebObjects/MZStoreServices.woa/ws/genres?id=26"
+
+genres = JSON.parse(Connect.get(genres_url).body)["26"]["subgenres"]
+
+genres.each do |_, genre|
+  parent = Category.create!(id: genre["id"], name: genre["name"],
+                           url: genre["rssUrls"]["topAudioPodcasts"])
+
+  if genre["subgenres"]
+    genre["subgenres"].each do |_, subgenre|
+      parent.subcategories.create!(id: subgenre["id"], name: subgenre["name"],
+                                  url: subgenre["rssUrls"]["topAudioPodcasts"])
+    end
+  end
+end
+
