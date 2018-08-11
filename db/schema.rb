f818250347147a8a8bcbf69b17eb696183f9c5c1
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-11 11:29:46 -0700

    Generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_173605) do
+ActiveRecord::Schema.define(version: 2018_08_11_182816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2018_08_05_173605) do
     t.text "url", null: false
     t.text "json", null: false
     t.index ["url"], name: "index_api_responses_on_url"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "parent_id"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "histories", force: :cascade do |t|
