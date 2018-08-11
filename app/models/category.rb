3d1b5f5f043d561744b96aa165fd2805ac89182d
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-11 12:03:24 -0700

    Add model category associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  belongs_to :category, foreign_key: "parent_id", optional: true
+  has_many :subcategories, class_name: "Category", foreign_key: "parent_id"
 end
