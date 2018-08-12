6a84909f442dc293867f46d396d42d8009ce6828
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-11 18:12:35 -0700

    Mark active categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
-module CategoriesHelper
+module CategoriesHelper 
+  def active_category(category, class_string)
+    class_string if current_page? category
+  end
+
+  def inactive_category(category, class_string)
+    class_string unless current_page? category
+  end
 end
