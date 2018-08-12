40bc878faceea7f0570c9316fefa01a08a199756
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-12 16:15:55 -0700

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
-  get 'history/create'
-  get 'history/destroy'
   root "pages#discover"
 
   devise_for :users
 
-  resource :subscriptions, only: %i[create destroy]
-  get "subscriptions", to: "subscriptions#index"
-
-  resource :histories, only: %i[create destroy update]
-  resource :cookies, only: %i[create destroy]
+  resources :subscriptions, only: %i[create destroy index]
+  resources :histories, only: %i[create destroy update]
+  resources :cookies, only: %i[create destroy]
+  resources :podcasts, only: %i[show]
+  resources :categories, only: %i[index show]
 
   get "about", to: "pages#about", as: "about"
   get "discover", to: "pages#discover", as: "discover"
   get "popular", to: "pages#popular", as: "popular"
   get "search", to: "pages#search", as: "search"
-  get "podcasts/:id", to: "podcasts#show", as: "podcast"
-  get "categories", to: "categories#index", as: "categories"
-  get "categories/:id", to: "categories#show", as: "category"
 end
