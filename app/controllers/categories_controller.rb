ed299852514be9035c5a2cb529dd4e3df9b7d477
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-15 11:35:46 -0700

    Create browse page for mobile

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.where(parent_id: nil)
+  end
+  
   def show
     @categories = Category.where(parent_id: nil)
     @category = Category.find(params[:id])
