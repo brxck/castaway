195c4983094a0a42e4b1a6bc0410b48fbbd66153
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-08-16 09:55:05 -0700

    Document itunes model

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -1,6 +1,10 @@
 class Itunes
+  # iTunes Search API:
+  # https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+
   BASE_URL = "https://itunes.apple.com/".freeze
 
+  # Returns search results matching term
   def self.search(term)
     url = BASE_URL + "search?" +
           { term: term, media: "podcast", country: "US" }.to_query
@@ -13,6 +17,7 @@ class Itunes
     results.map { |item| process_podcast(item) }
   end
 
+  # Returns podcast with id
   def self.lookup(id)
     url = BASE_URL + "lookup?id=#{id}"
     response = ApiResponse.cache(url, -> { 1.hour.ago }) do
@@ -23,7 +28,10 @@ class Itunes
     process_podcast(result)
   end
 
+  # Returns top podcasts in the genre with genre_id
   def self.genre(genre_id, count = 50)
+    # Undocumented search parameter genreId
+    # https://stackoverflow.com/a/27883886/8660199
     url = BASE_URL + "search?" +
           { term: "podcast", genreId: genre_id, limit: count, country: "US" }.to_query
 
@@ -35,6 +43,7 @@ class Itunes
     results.map { |item| process_podcast(item) }
   end
 
+  # Returns top podcasts on iTunes
   def self.toplist(count = 50)   
     rss = "https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/#{count}/explicit.json"
 
@@ -56,6 +65,7 @@ class Itunes
     end
   end
 
+  # Returns podcast object with properties accessible by dot notation
   def self.process_podcast(podcast)
     OpenStruct.new(
       itunes_id: podcast["collectionId"],
