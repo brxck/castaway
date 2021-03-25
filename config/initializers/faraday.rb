221004d1711035a9eb1b96ff05eb13f29cd58979
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-03-25 13:30:20 -0700

    Raise error on failing requests
    Caching bad requests is breaking things for cache duration

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,5 +1,7 @@
 # Global connection object for making http requests
 ::Connect = Faraday.new do |conn|
   conn.request  :url_encoded
+  conn.request  :retry, {max: 3, interval: 0.05}
+  conn.response  :raise_error
   conn.adapter  Faraday.default_adapter
 end
