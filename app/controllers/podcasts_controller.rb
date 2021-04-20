a96711f656d4d7e13d6a8439a2272c39744fb84f
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-04-20 02:50:58 +0000

    Render episode modal with Turbo frame

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -2,25 +2,35 @@ class PodcastsController < ApplicationController
   include Pagy::Backend
 
   def show
-    @podcast = Itunes.lookup(params[:id])
-    res = Connect.get(@podcast[:feed])
-    feed = Feed.parse(res.body)
-
-    # Grab description from feed because it's not included in iTunes look up
-    @podcast[:description] = feed[:description]
+    podcast, feed =
+      Rails
+        .cache
+        .fetch("show-podcast-#{params[:id]}", expires_in: 24.hours) do
+          podcast = Itunes.lookup(params[:id])
+          feed = Connect.get(podcast[:feed])
+          feed = Feed.parse(feed.body)
+          podcast[:description] = feed[:description]
+          [podcast, feed]
+        end
 
     if user_signed_in?
-      @episodes = Episode.with_histories(feed)
-      @subscribed =
-        current_user.subscriptions.where(itunes_id: params[:id]).any?
+      episodes = Episode.with_histories(feed)
+      subscribed = current_user.subscriptions.where(itunes_id: params[:id]).any?
     else
-      @episodes = Episode.from_feed(feed)
+      episodes = Episode.from_feed(feed)
     end
 
     # Handle episode and sort parameters
-    @modal_episode = @episodes[params[:episode_id]] if params[:episode_id]
-    @episodes = @episodes.reverse_each.to_h if params[:order] == "up"
+    modal_episode = episodes[params[:episode_id]] if params[:episode_id]
+    episodes = episodes.reverse_each.to_h if params[:order] == "up"
 
-    @pagy, @episodes = pagy_array(@episodes.values)
+    pagy, episodes = pagy_array(episodes.values)
+    render locals: {
+             podcast: podcast,
+             episodes: episodes,
+             modal_episode: modal_episode,
+             subscribed: subscribed,
+             pagy: pagy,
+           }
   end
 end
